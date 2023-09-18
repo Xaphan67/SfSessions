45f674e1bbecdfd4a2ef8aab9427a28b4bcdc6f8
Xaphan67 <cedric.falda@gmail.com>
2023-09-18 13:59:46 +0200

    Ajout : Entités

---
+++ migrations/Version20230918115045.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230918115045 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE categorie (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE formation (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE module (id INT AUTO_INCREMENT NOT NULL, programme_id INT NOT NULL, categorie_id INT NOT NULL, nom VARCHAR(50) NOT NULL, INDEX IDX_C24262862BB7AEE (programme_id), INDEX IDX_C242628BCF5E72D (categorie_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE programme (id INT AUTO_INCREMENT NOT NULL, modules_id INT NOT NULL, sessions_id INT NOT NULL, nb_jours INT NOT NULL, INDEX IDX_3DDCB9FF60D6DC42 (modules_id), INDEX IDX_3DDCB9FFF17C4D8C (sessions_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE session (id INT AUTO_INCREMENT NOT NULL, programme_id INT NOT NULL, formation_id INT NOT NULL, nom VARCHAR(50) NOT NULL, places INT NOT NULL, date_debut DATE NOT NULL, date_fin DATE NOT NULL, INDEX IDX_D044D5D462BB7AEE (programme_id), INDEX IDX_D044D5D45200282E (formation_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE session_stagiaire (session_id INT NOT NULL, stagiaire_id INT NOT NULL, INDEX IDX_C80B23B613FECDF (session_id), INDEX IDX_C80B23BBBA93DD6 (stagiaire_id), PRIMARY KEY(session_id, stagiaire_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE stagiaire (id INT AUTO_INCREMENT NOT NULL, prenom VARCHAR(50) NOT NULL, nom VARCHAR(50) NOT NULL, sexe VARCHAR(50) NOT NULL, date_naissance DATE NOT NULL, ville VARCHAR(100) NOT NULL, email VARCHAR(100) NOT NULL, telephone VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE module ADD CONSTRAINT FK_C24262862BB7AEE FOREIGN KEY (programme_id) REFERENCES programme (id)');
+        $this->addSql('ALTER TABLE module ADD CONSTRAINT FK_C242628BCF5E72D FOREIGN KEY (categorie_id) REFERENCES categorie (id)');
+        $this->addSql('ALTER TABLE programme ADD CONSTRAINT FK_3DDCB9FF60D6DC42 FOREIGN KEY (modules_id) REFERENCES module (id)');
+        $this->addSql('ALTER TABLE programme ADD CONSTRAINT FK_3DDCB9FFF17C4D8C FOREIGN KEY (sessions_id) REFERENCES session (id)');
+        $this->addSql('ALTER TABLE session ADD CONSTRAINT FK_D044D5D462BB7AEE FOREIGN KEY (programme_id) REFERENCES programme (id)');
+        $this->addSql('ALTER TABLE session ADD CONSTRAINT FK_D044D5D45200282E FOREIGN KEY (formation_id) REFERENCES formation (id)');
+        $this->addSql('ALTER TABLE session_stagiaire ADD CONSTRAINT FK_C80B23B613FECDF FOREIGN KEY (session_id) REFERENCES session (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE session_stagiaire ADD CONSTRAINT FK_C80B23BBBA93DD6 FOREIGN KEY (stagiaire_id) REFERENCES stagiaire (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE module DROP FOREIGN KEY FK_C24262862BB7AEE');
+        $this->addSql('ALTER TABLE module DROP FOREIGN KEY FK_C242628BCF5E72D');
+        $this->addSql('ALTER TABLE programme DROP FOREIGN KEY FK_3DDCB9FF60D6DC42');
+        $this->addSql('ALTER TABLE programme DROP FOREIGN KEY FK_3DDCB9FFF17C4D8C');
+        $this->addSql('ALTER TABLE session DROP FOREIGN KEY FK_D044D5D462BB7AEE');
+        $this->addSql('ALTER TABLE session DROP FOREIGN KEY FK_D044D5D45200282E');
+        $this->addSql('ALTER TABLE session_stagiaire DROP FOREIGN KEY FK_C80B23B613FECDF');
+        $this->addSql('ALTER TABLE session_stagiaire DROP FOREIGN KEY FK_C80B23BBBA93DD6');
+        $this->addSql('DROP TABLE categorie');
+        $this->addSql('DROP TABLE formation');
+        $this->addSql('DROP TABLE module');
+        $this->addSql('DROP TABLE programme');
+        $this->addSql('DROP TABLE session');
+        $this->addSql('DROP TABLE session_stagiaire');
+        $this->addSql('DROP TABLE stagiaire');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
