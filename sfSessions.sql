bae04004ecf3d59e27494850d99bfd6156d204fa
Xaphan67 <cedric.falda@gmail.com>
2023-09-20 20:32:05 +0200

    Mise à jour de la BDD

--- sfSessions.sql
+++ sfSessions.sql
@@ -14,11 +14,16 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Listage des données de la table sfsessions.categorie : ~0 rows (environ)
+-- Listage des données de la table sfsessions.categorie : ~4 rows (environ)
+INSERT INTO `categorie` (`id`, `nom`) VALUES
+	(1, 'Bureautique'),
+	(2, 'Infographie'),
+	(3, 'WebDesign'),
+	(4, 'Developpement Web');
 
 -- Listage des données de la table sfsessions.doctrine_migration_versions : ~1 rows (environ)
 INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
-	('DoctrineMigrations\\Version20230918115045', '2023-09-18 11:51:02', 584);
+	('DoctrineMigrations\\Version20230918115045', '2023-09-20 06:51:12', 365);
 
 -- Listage des données de la table sfsessions.formation : ~3 rows (environ)
 INSERT INTO `formation` (`id`, `nom`) VALUES
@@ -28,9 +33,24 @@ INSERT INTO `formation` (`id`, `nom`) VALUES
 
 -- Listage des données de la table sfsessions.messenger_messages : ~0 rows (environ)
 
--- Listage des données de la table sfsessions.module : ~0 rows (environ)
+-- Listage des données de la table sfsessions.module : ~8 rows (environ)
+INSERT INTO `module` (`id`, `categorie_id`, `nom`) VALUES
+	(1, 1, 'Word'),
+	(2, 1, 'Excel'),
+	(3, 1, 'Outlook'),
+	(4, 2, 'Photoshop'),
+	(5, 2, 'Illustrator'),
+	(6, 2, 'InDesign'),
+	(7, 3, 'Figma'),
+	(8, 4, 'HTML');
 
--- Listage des données de la table sfsessions.programme : ~0 rows (environ)
+-- Listage des données de la table sfsessions.programme : ~6 rows (environ)
+INSERT INTO `programme` (`id`, `nb_jours`, `module_id`, `session_id`) VALUES
+	(1, 2, 1, 5),
+	(2, 2, 2, 5),
+	(3, 2, 3, 5),
+	(4, 4, 4, 5),
+	(5, 4, 5, 5);
 
 -- Listage des données de la table sfsessions.session : ~6 rows (environ)
 INSERT INTO `session` (`id`, `formation_id`, `nom`, `places`, `date_debut`, `date_fin`) VALUES
@@ -38,14 +58,17 @@ INSERT INTO `session` (`id`, `formation_id`, `nom`, `places`, `date_debut`, `dat
 	(2, 1, 'Initiation à Word et Excel', 8, '2023-06-17', '2023-06-29'),
 	(3, 1, 'Initiation Infographie (PS, INDD, AI)', 10, '2023-06-17', '2023-07-20'),
 	(4, 1, 'Perfectionnement Word, Excel, Powerpoint', 6, '2023-07-08', '2023-07-12'),
-	(5, 1, 'Initiation Bureautoque et infographie', 10, '2023-07-12', '2023-08-07'),
+	(5, 1, 'Initiation Bureautique et infographie', 10, '2023-07-12', '2023-08-07'),
 	(6, 1, 'Initiation en PHP / SQL', 12, '2023-09-01', '2023-12-12');
 
--- Listage des données de la table sfsessions.session_stagiaire : ~0 rows (environ)
+-- Listage des données de la table sfsessions.session_stagiaire : ~1 rows (environ)
+INSERT INTO `session_stagiaire` (`session_id`, `stagiaire_id`) VALUES
+	(5, 1);
 
--- Listage des données de la table sfsessions.stagiaire : ~1 rows (environ)
+-- Listage des données de la table sfsessions.stagiaire : ~2 rows (environ)
 INSERT INTO `stagiaire` (`id`, `prenom`, `nom`, `sexe`, `date_naissance`, `ville`, `email`, `telephone`) VALUES
-	(1, 'Cédric', 'FALDA', 'H', '1987-07-29', 'HANGENBIETEN', 'cedric.falda@gmail.com', '0604190584');
+	(1, 'Cédric', 'FALDA', 'M', '1987-07-29', 'HANGENBIETEN', 'cedric.falda@gmail.com', '0604190584'),
+	(2, 'Baisangour', 'Aliev', 'M', '1997-07-29', 'STRASBOURG', 'aliev@exemple.com', '0102030405');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
