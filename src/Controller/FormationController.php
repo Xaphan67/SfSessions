71c7d350147002008d6e8638e4e19f2d9dfb05d1
Xaphan67 <cedric.falda@gmail.com>
2023-09-21 13:50:16 +0200

    Ajout : Formations

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -2,17 +2,59 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Formation;
+use App\Form\FormationType;
+use App\Repository\FormationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FormationController extends AbstractController
 {
-    #[Route('/formation', name: 'app_formation')]
-    public function index(): Response
+    #[Route('/formations', name: 'app_formation')]
+    public function index(FormationRepository $formationRepository): Response
     {
+
+        // Récupère toutes les formations et les classe dans l'ordre alphabétique de leur nom
+        $formations = $formationRepository->findBy([], ["nom" => "ASC"]);
+
         return $this->render('formation/index.html.twig', [
-            'controller_name' => 'FormationController',
+            'formations' => $formations,
+        ]);
+    }
+
+    #[Route('/formation/new', name: 'new_formation')]
+    #[Route('/formation/edit/{id}', name: 'edit_formation')]
+    public function new_edit(Formation $formation = null, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // Instancie une nouvelle formation lors d'un ajout
+        if (!$formation) {
+            $formation = new Formation();
+        }
+
+        // Instancie un formulaire de type Formation
+        $form = $this->createForm(FormationType::class, $formation);
+
+        $form->handleRequest($request);
+
+        // Traitement du formulaire s'il est soumis et valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formation = $form->getData();
+            // Prpare PDO
+            $entityManager->persist($formation);
+            // Execute PDO
+            $entityManager->flush();
+
+            // Redirige vers la liste des formations
+            return $this->redirectToRoute('app_formation');
+        }
+
+        return $this->render('formation/new.html.twig', [
+            'formAddFormation' => $form,
+            'edit' => $formation->getId(),
+            'formation' => $formation
         ]);
     }
 }
