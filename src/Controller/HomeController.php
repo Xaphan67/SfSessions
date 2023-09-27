f50076dea225f4b8ed625400b664cce50ff57880
Xaphan67 <cedric.falda@gmail.com>
2023-09-27 17:00:06 +0200

    Ajout : Redirection vers homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+    #[Route('/', name: 'app')]
     #[Route('/home', name: 'app_home')]
     public function index(SessionRepository $sessionRepository): Response
     {
